e19c9127f82e2014280ca1d03d10f8a429555fac
Evan Benn <evanbenn@gmail.com>
2015-02-23 17:43:31 +1100

    some corrections, still not working

--- kicker.py
+++ kicker.py
@@ -8,46 +8,54 @@ from willie.module import commands, interval
 from willie.config import ConfigurationError
 from willie.logger import get_logger
 
+
 def setup(bot):
     bot.memory['kicker_manager'] = KickerManager(bot)
+
 
 @commands('kicker')
 def kicker_command(bot, trigger):
     """Manage RSS feeds. For a list of commands, type: .rss help"""
     bot.memory['kicker_manager'].kicker_command(bot, trigger)
 
+
 def do_ranker(players):
-	ELO_ranker(players)
+    ELO_ranker(players)
+
 
 def basic_ranker(players):
-	for p in players[0:2]:
-		players.rank += 1
-	for p in players[2:4]
-		players.rank -= 1
+    for p in players[0:2]:
+        players.rank += 1
+    for p in players[2:4]:
+        players.rank -= 1
+
 
 def ELO_ranker(players):
-	"https://metinmediamath.wordpress.com/2013/11/27/how-to-calculate-the-elo-rating-including-example/"
-	K = 50
+    "https://metinmediamath.wordpress.com/2013/11/27/how-to-calculate-the-elo-rating-including-example/"
+    K = 50
 
-	r1 = (players[0].rank * players[1].rank) ** 0.5
-	r2 = (players[2].rank * players[3].rank) ** 0.5
-	R1 = 10 ** (r1/400)
-	R2 = 10 ** (r2/400)
-	E1 = R1 / (R1 + R2)
-	E2 = R2 / (R1 + R2)
-	rd1 = r1 + K * (1 - E1)
-	rd2 = r2 + K * (0 - E2)
+    r1 = (players[0].rank * players[1].rank) ** 0.5
+    r2 = (players[2].rank * players[3].rank) ** 0.5
+    R1 = 10 ** (r1 / 400)
+    R2 = 10 ** (r2 / 400)
+    E1 = R1 / (R1 + R2)
+    E2 = R2 / (R1 + R2)
+    rd1 = r1 + K * (1 - E1)
+    rd2 = r2 + K * (0 - E2)
 
-	delta1 = (rd1 ** 2 - players[0].rank * players[1].rank) / (players[0].rank + players[1].rank)
-	delta2 = (rd2 ** 2 - players[2].rank * players[3].rank) / (players[2].rank + players[3].rank)
+    delta1 = (
+        rd1 ** 2 - players[0].rank * players[1].rank) / (players[0].rank + players[1].rank)
+    delta2 = (
+        rd2 ** 2 - players[2].rank * players[3].rank) / (players[2].rank + players[3].rank)
 
-	players[0].rank += delta1
-	players[1].rank += delta1
-	players[2].rank += delta2
-	players[3].rank += delta2
+    players[0].rank += delta1
+    players[1].rank += delta1
+    players[2].rank += delta2
+    players[3].rank += delta2
 
 
 class KickerManager:
+
     def __init__(self, bot):
         self.players = {}
         self.games = {}
@@ -55,10 +63,10 @@ class KickerManager:
         self.events = []
 
     def _add_event(self, event, print_reply=False):
-    	self.events.append(event)
-        reply = self.events[-1].processEvent(players, games)
-        if print_reply:
-        	self.bot.say(reply)
+        self.events.append(event)
+        reply = self.events[-1].processEvent(self.players, self.games)
+        # if print_reply:
+        # self.bot.say(reply)
 
     def _add_player(self, name):
         self._add_event(AddPlayerEvent(name), print_reply=True)
@@ -73,7 +81,8 @@ class KickerManager:
         ladder = sorted(self.players, key=lambda x: x.rank)
         count = 1
         for p in ladder:
-        	bot.say("{}: {}, {}".format(count++, p.name, p.rank))
+            bot.say("{}: {}, {}".format(count, p.name, p.rank))
+            count += 1
 
     def kicker_command(self, bot, trigger):
         text = trigger.group().split()
@@ -82,7 +91,7 @@ class KickerManager:
             self._add_player(text[2])
 
         elif text[1] == 'game':
-            self._add_game(KickerGame(text[1:]))
+            self._add_game(text[2:])
 
         elif text[1] == 'ladder':
             self._show_ladder(bot)
@@ -92,16 +101,21 @@ class KickerManager:
 
 
 class KickerPlayer:
+
     def __init__(self, name):
         self.name = name
         self.deleted = False
         self.rank = 1000
 
-    def delete():
-    	self.deleted = True
+    def delete(self):
+        self.deleted = True
+
+    def __str__(self):
+        return 'Player: {}, {}'.format(self.name, self.rank)
 
 
 class KickerGame:
+
     def __init__(self, commandString):
         self.command = commandString
 
@@ -113,52 +127,70 @@ class KickerGame:
 
         self.deleted = False
 
-    def delete():
-    	self.deleted = True
+    def delete(self):
+        self.deleted = True
+
+    def __str__(self):
+        return 'Game:\n\t{}\n\t{}\n\tbeat\n\t{}\n\t{}'.format(
+            str(
+                self.players[0]), str(
+                self.players[0]), str(
+                self.players[0]), str(
+                    self.players[0]))
+
 
 class KickerEvent:
-	def processEvent(self, players, games):
-		pass
+
+    def processEvent(self, players, games):
+        pass
+
 
 class AddPlayerEvent(KickerEvent):
-	def __init__(self, player):
-		self.player = player
 
-	def processEvent(self, players, games):
-		players[self.player] = KickerPlayer(name)
-		return 'added: ' + kwargs['player']
+    def __init__(self, player):
+        self.player = KickerPlayer(player)
+
+    def processEvent(self, players, games):
+        players[self.player.name] = self.player
+        return 'added: ' + str(self.player)
+
 
 class DelPlayerEvent(KickerEvent):
-	def __init__(self, player):
-		self.player = player
 
-	def processEvent(self, players, games):
-		players[self.player].delete()
-		return 'deleted: ' + kwargs['player']
+    def __init__(self, player):
+        self.player = player
+
+    def processEvent(self, players, games):
+        players[self.player].delete()
+        return 'deleted: ' + str(self.player)
+
 
 class AddGameEvent(KickerEvent):
-	def __init__(self, command):
-		self.command = command
-		self.game = KickerGame(command)
 
-	def processEvent(self, players, games):
-		real_players = []
-		for p in self.game.players:
-			if p not in players:
-				return 'fail'
-			real_players.append(players[p])
+    def __init__(self, command):
+        self.command = command
+        self.game = KickerGame(command)
 
-		do_ranker(real_players)
+    def processEvent(self, players, games):
+        real_players = []
+        for p in self.game.players:
+            if p not in players:
+                print p
+                print players
+                return 'fail'
+            real_players.append(players[p])
 
-		games.KickerGame[len(self.games)+1] = self.game
-		return 'add game: ' + self.game)
+        do_ranker(real_players)
+
+        games[len(games) + 1] = self.game
+        return 'add game: ' + str(self.game)
+
 
 class DelGameEvent(KickerEvent):
-	def __init__(self, number):
-		self.number = number
 
-	def processEvent(self, players, games):
-		games[number].delete()
-		return 'del game: ' + games[number]
+    def __init__(self, number):
+        self.number = number
 
-
+    def processEvent(self, players, games):
+        games[self.number].delete()
+        return 'del game: ' + games[self.number]
