3c6be7a2d5810b5ae71e3abf3f5d4afd96ced577
EvanTheB <evanbenn@gmail.com>
2015-03-28 21:05:50 +1100

    fixed some trueskill bugs, removed odd numerical code

--- trueskill.py
+++ trueskill.py
@@ -27,13 +27,36 @@ DYNAMICS_FACTOR = 25.0 / 300
 import math
 
 
+def chances(team_a, team_b):
+
+    draw_margin = get_draw_margin_from_draw_probability(
+        DRAW_PROBABILITY, BETA, len(team_a) + len(team_b))
+
+    c = math.sqrt(sum([p.sigma ** 2 for p in team_a]) +
+                  sum([p.sigma ** 2 for p in team_b]) +
+                  len(team_a + team_b) * BETA ** 2)
+    mean_a = sum([p.mu for p in team_a])
+    mean_b = sum([p.mu for p in team_b])
+    mean_delta = mean_a - mean_b
+
+    mean_delta /= c
+    draw_margin /= c
+    p_win = gaussian_cumulative_to(mean_delta - abs(draw_margin))
+    p_loss = gaussian_cumulative_to(-mean_delta - abs(draw_margin))
+
+    p_draw = (gaussian_cumulative_to(abs(draw_margin) - abs(mean_delta))
+              - gaussian_cumulative_to(-abs(draw_margin) - abs(mean_delta)))
+
+    print p_win, p_draw, p_loss, p_win+ p_draw+ p_loss
+
+
 def calculate_nvn(team_a, team_b, score_a, score_b):
     """
     Calculates new trueskills for a two team game.
     Scores are translated to win/loss/draw.
     Teams are lists of players with 'mu' and 'sigma'
     """
-    draw_margin = get_draw_margin_from_draw_probability(DRAW_PROBABILITY, BETA)
+    draw_margin = get_draw_margin_from_draw_probability(DRAW_PROBABILITY, BETA, len(team_a) + len(team_b))
 
     c = math.sqrt(sum([p.sigma ** 2 for p in team_a]) +
                   sum([p.sigma ** 2 for p in team_b]) +
@@ -80,7 +103,7 @@ def calculate_1v1(team_a, team_b, score_a, score_b):
     Scores are translated to win/loss/draw.
     players have 'mu' and 'sigma'
     """
-    draw_margin = get_draw_margin_from_draw_probability(DRAW_PROBABILITY, BETA)
+    draw_margin = get_draw_margin_from_draw_probability(DRAW_PROBABILITY, BETA, 2)
     c = math.sqrt(team_a.sigma ** 2 +
                   team_b.sigma ** 2 +
                   2 * BETA ** 2)
@@ -199,9 +222,9 @@ def gaussian_at(x, mean=0.0, standard_dev=1.0):
     # // 1 -(x-mean)^2 / (2*stdDev^2)
     # // P(x) = ------------------- * e
     # // stdDev * sqrt(2*pi)
-    multiplier = 1.0 / (standard_dev * (2 * math.pi) ** 0.5)
+    multiplier = 1.0 / (standard_dev * math.sqrt(2 * math.pi))
     exp_part = math.exp(
-        (-1.0 * math.pow(x - mean, 2.0)) / (2 * (standard_dev ** 2)))
+        (-1.0 * (x - mean) ** 2 / (2 * (standard_dev ** 2))))
     result = multiplier * exp_part
     return result
 
@@ -209,87 +232,92 @@ def gaussian_at(x, mean=0.0, standard_dev=1.0):
 def gaussian_cumulative_to(x, mean=0.0, standard_dev=1.0):
     """
     cumulative (error function) to x.
-    did not implement non standard distributions woops
     """
+    x = (x - mean) / standard_dev
     return 0.5 + 0.5 * math.erf(x / math.sqrt(2))
 
 
-def error_function_cumulative_to(x):
-    """
-    What the heck is this
-    """
-    # // Derived from page 265 of Numerical Recipes 3rd Edition
-    z = abs(x)
-    t = 2.0 / (2.0 + z)
-    ty = 4 * t - 2
-    coefficients = [
-        -1.3026537197817094, 6.4196979235649026e-1,
-        1.9476473204185836e-2, -9.561514786808631e-3, -9.46595344482036e-4,
-        3.66839497852761e-4, 4.2523324806907e-5, -2.0278578112534e-5,
-        -1.624290004647e-6, 1.303655835580e-6, 1.5626441722e-8, -
-        8.5238095915e-8,
-        6.529054439e-9, 5.059343495e-9, -9.91364156e-10, -2.27365122e-10,
-        9.6467911e-11, 2.394038e-12, -6.886027e-12, 8.94487e-13, 3.13092e-13,
-        -1.12708e-13, 3.81e-16, 7.106e-15, -
-        1.523e-15, -9.4e-17, 1.21e-16, -2.8e-17
-    ]
-    ncof = len(coefficients)
-    d = 0.0
-    dd = 0.0
-    for j in reversed(coefficients):
-        tmp = d
-        d = ty * d - dd + j
-        dd = tmp
-    ans = t * math.exp(-z * z + 0.5 * (coefficients[0] + ty * d) - dd)
-    return ans if x >= 0.0 else 2.0 - ans
+# def error_function_cumulative_to(x):
+#     """
+#     What the heck is this
+#     """
+#     # // Derived from page 265 of Numerical Recipes 3rd Edition
+#     z = abs(x)
+#     t = 2.0 / (2.0 + z)
+#     ty = 4 * t - 2
+#     coefficients = [
+#         -1.3026537197817094, 6.4196979235649026e-1,
+#         1.9476473204185836e-2, -9.561514786808631e-3, -9.46595344482036e-4,
+#         3.66839497852761e-4, 4.2523324806907e-5, -2.0278578112534e-5,
+#         -1.624290004647e-6, 1.303655835580e-6, 1.5626441722e-8, -
+#         8.5238095915e-8,
+#         6.529054439e-9, 5.059343495e-9, -9.91364156e-10, -2.27365122e-10,
+#         9.6467911e-11, 2.394038e-12, -6.886027e-12, 8.94487e-13, 3.13092e-13,
+#         -1.12708e-13, 3.81e-16, 7.106e-15, -
+#         1.523e-15, -9.4e-17, 1.21e-16, -2.8e-17
+#     ]
+#     ncof = len(coefficients)
+#     d = 0.0
+#     dd = 0.0
+#     for j in reversed(coefficients):
+#         tmp = d
+#         d = ty * d - dd + j
+#         dd = tmp
+#     ans = t * math.exp(-z * z + 0.5 * (coefficients[0] + ty * d) - dd)
+#     return ans if x >= 0.0 else 2.0 - ans
 
 
-def get_draw_margin_from_draw_probability(draw_probability, beta):
+def get_draw_margin_from_draw_probability(draw_probability, beta, n_players):
     """
     From the % chance of draw get the magic number
     """
+    denom = beta * math.sqrt(n_players)
 
-    # // Derived from TrueSkill technical report (MSR-TR-2006-80), page 6
-    # // draw probability = 2 * CDF(margin/(sqrt(n1+n2)*beta)) -1
-    # // implies
-    # //
-    # // margin = inversecdf((draw probability + 1)/2) * sqrt(n1+n2) * beta
-    # // n1 and n2 are the number of players on each team
-    margin = gaussian_inverse_cumulative_to(
-        .5 * (draw_probability + 1), 0, 1) * math.sqrt(1 + 1) * beta
-    return margin
+    hi = 10.
+    lo = -10.
 
+    while abs(hi - lo) > 0.00001:
+        mid = (hi + lo) / 2
+        dp = 2 * gaussian_cumulative_to(mid / denom) - 1
 
-def gaussian_inverse_cumulative_to(x, mean=0.0, standard_dev=1.0):
-    """
-    This is weird
-    """
-    # // From numerical recipes, page 320
-    return mean - math.sqrt(2) * standard_dev * inverse_error_function_cumulative_to(2 * x)
+        if dp < draw_probability:
+            lo = mid
+        else:
+            hi = mid
+    return mid
 
 
-def inverse_error_function_cumulative_to(p):
-    # // From page 265 of numerical recipes
-    if p >= 2.0:
+# def gaussian_inverse_cumulative_to(x, mean=0.0, standard_dev=1.0):
+#     """
+#     This is weird
+#     """
+# // From numerical recipes, page 320
+# return mean - math.sqrt(2) * standard_dev *
+# inverse_error_function_cumulative_to(2 * x)
 
-        return -100
 
-    if p <= 0.0:
+# def inverse_error_function_cumulative_to(p):
+# // From page 265 of numerical recipes
+#     if p >= 2.0:
 
-        return 100
+#         return -100
 
-    pp = p if p < 1.0 else 2 - p
-    # // Initial guess
-    t = math.sqrt(-2 * math.log(pp / 2.0))
-    x = -0.70711 * \
-        ((2.30753 + t * 0.27061) / (1.0 + t * (0.99229 + t * 0.04481)) - t)
-    for j in range(2):
+#     if p <= 0.0:
 
-        err = error_function_cumulative_to(x) - pp
-        # // Halley
-        x += err / (1.12837916709551257 * math.exp(-(x * x)) - x * err)
+#         return 100
 
-    return x if p < 1.0 else -x
+#     pp = p if p < 1.0 else 2 - p
+# // Initial guess
+#     t = math.sqrt(-2 * math.log(pp / 2.0))
+#     x = -0.70711 * \
+#         ((2.30753 + t * 0.27061) / (1.0 + t * (0.99229 + t * 0.04481)) - t)
+#     for j in range(2):
+
+#         err = error_function_cumulative_to(x) - pp
+# // Halley
+#         x += err / (1.12837916709551257 * math.exp(-(x * x)) - x * err)
+
+#     return x if p < 1.0 else -x
 
 
 def tester(a, b, a_s, b_s, message):
