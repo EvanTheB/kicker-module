ff95d3f49348ea3d982e011ab466a515562ab5f5
Evan Benn <evanbenn@gmail.com>
2015-12-30 14:20:50 +1100

    main use commandline log

--- wrank/__main__.py
+++ wrank/__main__.py
@@ -2,5 +2,6 @@ import wrank
 import sys
 
 if __name__ == '__main__':
-    k = wrank.LadderManager("kicker.log")
-    print "\n".join(k.ladder_command(sys.argv[1:]))
+    k = wrank.LadderManager(sys.argv[1])
+    print "\n".join(k.ladder_command(sys.argv[2:]))
+
