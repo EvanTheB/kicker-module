cf7fcd588e047248b961db69cccc7bfe17d1baa9
EvanTheB <evanbenn@gmail.com>
2016-03-14 21:14:28 +1100

    cruft removal service

--- wrank/ladder/trueskill.py
+++ wrank/ladder/trueskill.py
@@ -156,48 +156,6 @@ def calculate_nvn(team_a, team_b, was_win):
         team_b, skill_delta_b, not was_win, False)
 
 
-def calculate_1v1(team_a, team_b, score_a, score_b):
-    """
-    Calculates new trueskills for a two player game.
-    Scores are translated to win/loss/draw.
-    players have 'mu' and 'sigma'
-    """
-    draw_margin = get_draw_margin_from_draw_probability(
-        DRAW_PROBABILITY, BETA, 2)
-    c = math.sqrt(team_a.sigma ** 2 +
-                  team_b.sigma ** 2 +
-                  2 * BETA ** 2)
-
-    mean_delta = ((team_a.mu - team_b.mu) if score_a > score_b
-                  else (team_b.mu - team_a.mu))
-
-    def update_rating(team, mean_delta, was_draw, winner):
-        """
-        helper for doing the maths per team.
-        """
-        rank_multiplier = 1.0 if winner else -1.0
-        if not was_draw:
-            v = v_exceeds_margin(mean_delta, draw_margin, c)
-            w = w_exceeds_margin(mean_delta, draw_margin, c)
-
-        else:
-            v = v_within_margin(mean_delta, draw_margin, c)
-            w = w_within_margin(mean_delta, draw_margin, c)
-            rank_multiplier = 1.0
-
-        mean_multiplier = (team.sigma ** 2 + DYNAMICS_FACTOR ** 2) / c
-        variance_with_dynamics = team.sigma ** 2 + DYNAMICS_FACTOR ** 2
-        std_dev_multiplier = variance_with_dynamics / (c ** 2)
-        new_mean = team.mu + (rank_multiplier * mean_multiplier * v)
-        new_std_dev = math.sqrt(
-            variance_with_dynamics * (1 - w * std_dev_multiplier))
-        team.mu = new_mean
-        team.sigma = new_std_dev
-
-    update_rating(team_a, mean_delta, score_a == score_b, score_a > score_b)
-    update_rating(team_b, mean_delta, score_a == score_b, score_b > score_a)
-
-
 def v_exceeds_margin(team_performance_difference, draw_margin, c=1.0):
     """
     The V function for a win
@@ -300,36 +258,6 @@ def gaussian_cumulative_to(x, mean=0.0, standard_dev=1.0):
     return 0.5 + 0.5 * math.erf(x / math.sqrt(2))
 
 
-# def error_function_cumulative_to(x):
-#     """
-#     What the heck is this
-#     """
-# // Derived from page 265 of Numerical Recipes 3rd Edition
-#     z = abs(x)
-#     t = 2.0 / (2.0 + z)
-#     ty = 4 * t - 2
-#     coefficients = [
-#         -1.3026537197817094, 6.4196979235649026e-1,
-#         1.9476473204185836e-2, -9.561514786808631e-3, -9.46595344482036e-4,
-#         3.66839497852761e-4, 4.2523324806907e-5, -2.0278578112534e-5,
-#         -1.624290004647e-6, 1.303655835580e-6, 1.5626441722e-8, -
-#         8.5238095915e-8,
-#         6.529054439e-9, 5.059343495e-9, -9.91364156e-10, -2.27365122e-10,
-#         9.6467911e-11, 2.394038e-12, -6.886027e-12, 8.94487e-13, 3.13092e-13,
-#         -1.12708e-13, 3.81e-16, 7.106e-15, -
-#         1.523e-15, -9.4e-17, 1.21e-16, -2.8e-17
-#     ]
-#     ncof = len(coefficients)
-#     d = 0.0
-#     dd = 0.0
-#     for j in reversed(coefficients):
-#         tmp = d
-#         d = ty * d - dd + j
-#         dd = tmp
-#     ans = t * math.exp(-z * z + 0.5 * (coefficients[0] + ty * d) - dd)
-#     return ans if x >= 0.0 else 2.0 - ans
-
-
 def get_draw_margin_from_draw_probability(draw_probability, beta, n_players):
     """
     From the % chance of draw get the magic number
@@ -347,90 +275,3 @@ def get_draw_margin_from_draw_probability(draw_probability, beta, n_players):
         else:
             hi = mid
     return mid
-
-
-# def gaussian_inverse_cumulative_to(x, mean=0.0, standard_dev=1.0):
-#     """
-#     This is weird
-#     """
-# // From numerical recipes, page 320
-# return mean - math.sqrt(2) * standard_dev *
-# inverse_error_function_cumulative_to(2 * x)
-
-
-# def inverse_error_function_cumulative_to(p):
-# // From page 265 of numerical recipes
-#     if p >= 2.0:
-
-#         return -100
-
-#     if p <= 0.0:
-
-#         return 100
-
-#     pp = p if p < 1.0 else 2 - p
-# // Initial guess
-#     t = math.sqrt(-2 * math.log(pp / 2.0))
-#     x = -0.70711 *
-#         ((2.30753 + t * 0.27061) / (1.0 + t * (0.99229 + t * 0.04481)) - t)
-#     for j in range(2):
-
-#         err = error_function_cumulative_to(x) - pp
-# // Halley
-#         x += err / (1.12837916709551257 * math.exp(-(x * x)) - x * err)
-
-#     return x if p < 1.0 else -x
-
-
-def tester(a, b, a_s, b_s, message):
-    calculate_1v1(a, b, a_s, b_s)
-    print message
-    print a
-    print b
-    print
-
-
-def tester_team(a, b, a_s, b_s, message):
-    calculate_NvN(a, b, a_s, b_s)
-    print message
-    print repr(a)
-    print b
-    print
-
-if __name__ == "__main__":
-    class Player():
-
-        def __init__(self, mu=25.0, sigma=25.0 / 3):
-            self.mu = float(mu)
-            self.sigma = float(sigma)
-
-        def __str__(self):
-            return "{0.mu} {0.sigma}".format(self)
-
-        def __repr__(self):
-            return self.__str__()
-
-    a = Player()
-    b = Player()
-
-
-# Draw
-    tester(a, b, 1, 1, "draw")
-# win
-    tester(a, b, 2, 0, "win")
-# win
-    tester(a, b, 0, 2, "loss")
-
-# High skill
-    tester(Player(mu=35), Player(mu=15), 1, 1, "high skill draw")
-    tester(Player(mu=35), Player(mu=15), 2, 0, "high skill win")
-# High confidence
-    tester(Player(sigma=1), Player(sigma=1), 1, 1, "high confidence draw")
-    tester(Player(sigma=1), Player(sigma=1), 2, 0, "high confidence win")
-
-# Test team
-    a = [Player()] * 2 + [Player(mu=35)]
-    b = [Player()] * 2 + [Player(mu=35)]
-    tester_team(a, b, 1, 1, "team draw")
-    tester_team(a, b, 2, 0, "team win")
-    tester_team(a, b, 0, 2, "team loss")
